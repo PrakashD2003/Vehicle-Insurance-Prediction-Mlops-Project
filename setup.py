c76c34126c36f63a37c55a4d789f15d9d53d036f
Prakash <prakash.dwivedi2003@gmail.com>
2025-04-14 14:03:36 +0530

    1.Projected Structure Created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="scr",
+    version="0.0.1",
+    author="Prakash Dwivedi",
+    author_email="prakash.dwivedi2003@gmail.com",
+    packages=find_packages()
+)
